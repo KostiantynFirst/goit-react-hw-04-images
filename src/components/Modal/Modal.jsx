87321d5d727710578ae86116c90546a2e2ddcd8d
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-11 12:00:20 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -24,18 +24,21 @@ useEffect (() => {
 
 }, [selectedImage])
 
+
+useEffect(() => {
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
       onClose();
     }
   };
 
-  (() => {
-    document.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keydown', handleKeyDown);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
-})();
+    
+}, [onClose])
+
 
   const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
